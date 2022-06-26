ced656a8976ff510b3c3bc7820b2045b77b48e75
ElizaSouza <eliza_cristina20@outlook.com>
2022-06-26 16:21:38 -0300

    Projeto de Urna eletrônica usando JavaScript e stylus

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'Cleiton',
+                partido: 'PT',
+                fotos: [
+                    {url:'ato1.jpeg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '77222',
+                nome: 'Elizabete',
+                partido: 'PSDB',
+                fotos: [
+                    {url:'ato2.jpeg', legenda: 'Vereador'}
+                ]
+            }
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos:[
+            {
+                numero: '23',
+                nome: 'Eliete',
+                partido: 'ABC',
+                vice: 'Eli',
+                fotos: [
+                    {url:'ato2.jpeg', legenda: 'Prefeito'},
+                    {url:'ato1.jpeg', legenda: 'Vice-Prefeito', small: true} 
+                ]
+            },
+            {
+                numero: '99',
+                nome: 'Teste',
+                partido: 'ABC',
+                vice: 'Eli',
+                fotos: [
+                    {url:'ato2.jpeg', legenda: 'Prefeito'},
+                    {url:'ato1.jpeg', legenda: 'Vice-Prefeito', small: true} 
+                ]
+            }
+        ] 
+    }
+];
